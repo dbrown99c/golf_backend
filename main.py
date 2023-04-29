0ab2c90190ff57481a01183899d4b43f88f36a92
dbrown99c <dbrown99c@gmail.com>
2023-04-29 17:34:22 -0400

    changed serving port

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import uvicorn
 def serve_api():
     """This function targets api_endpoints.py as an application to host on uvicorns ASGI."""
     if __name__ == "__main__":
-        uvicorn.run("api_endpoints:app", host="0.0.0.0", port=80, log_level="error")
+        uvicorn.run("api_endpoints:app", host="0.0.0.0", port=3002, log_level="error")
 
 
 serve_api()
